9b98f676289b129d0a2e4ecf6559ba5b7fd9b485
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-07-07 01:24:33 +0300

    Initial Commit

---
+++ SwiftUIStacks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIStacks
+//
+//  Created by Abdelrahman Mohamed on 7.07.2020.
+//  Copyright © 2020 Abdelrahman Mohamed. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
