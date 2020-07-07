c2c2f6c4f139fd33bca22559dd82bebc2424f314
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-07-08 00:33:55 +0300

    Update ContentView.swift
    Co-Authored-By: Abdelrahman Mohamed <obada.semary@gmail.com>

--- SwiftUIStacks/ContentView.swift
+++ SwiftUIStacks/ContentView.swift
@@ -25,6 +25,7 @@ struct ContentView: View {
                         .foregroundColor(.white)
                         .padding(5)
                         .background(Color(red: 255/255, green: 183/255, blue: 37/255))
+                        .cornerRadius(10)
                         .offset(x: 0, y: 87)
                 }
             }
@@ -40,6 +41,7 @@ struct ContentView: View {
                     .foregroundColor(.white)
                     .padding(5)
                     .background(Color(red: 255/255, green: 183/255, blue: 37/255))
+                    
                     .offset(x: 0, y: 105)
             }
 
